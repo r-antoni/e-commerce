3dfe20cc0e0c6bd418e1bdcef479a32b34b947dd
r-antoni <reindraantoni@gmail.com>
2023-07-20 06:19:33 +0700

    Adding error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,7 +4,8 @@ const Error = () => {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center gap-10">
         <p className="text-3xl font-bold">404 Not Found</p>
-        <p className="text-xl">Sorry we can't find the page that you're looking for</p>
+        <p className="text-xl font-semibold">Uh Oh !!!</p>
+        <p className="text-xl"> Sorry we can't find the page that you're looking for</p>
         <button className="bg-cyan-800 text-white text-xl px-5 py-3 rounded-lg">
             <Link to="/">Go back to homepage</Link>
         </button>
